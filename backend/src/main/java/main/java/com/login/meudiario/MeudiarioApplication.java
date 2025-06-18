df3a14d997aa426adec94f651d76ccba112683e1
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-06-18 11:33:29 -0300

    backend: Adding SpringBoot to backend

---
+++ backend/src/main/java/main/java/com/login/meudiario/MeudiarioApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.login.meudiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeudiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeudiarioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/main/java/com/login/meudiario/MeudiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package main.java.com.login.meudiario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeudiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
